cdd17d1dfd8a702bf1b258ee73a4dc7028fb3ba5
vovchenko91 <denisvovchenko1991@gmail.com>
2017-11-03 15:01:03 +0200

    First commit

---
+++ src/main/java/buglava/buglava/BuglavaApplication.java
@@ -0,0 +1,12 @@
+package buglava.buglava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuglavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuglavaApplication.class, args);
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (1, true, 'Manufacturer0', 0, 'Watch0', 99.99);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (2, false, 'Manufacturer1', 1, 'Watch1', 123.22);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (3, true, 'Manufacturer4', 2, 'Watch2', 155.00);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (4, false, 'Manufacturer9', 3, 'Watch3', 67.11);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (5, true, 'Manufacturer16', 0, 'Watch4', 109.23);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (6, false, 'Manufacturer25', 1, 'Watch5', 240.23);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (7, true, 'Manufacturer36', 1, 'Watch6', 187.66);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (8, false, 'Manufacturer49', 0, 'Watch7', 66.00);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (9, true, 'Manufacturer64', 3, 'Watch8', 123.78);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (10, false, 'Manufacturer81', 2, 'Watch9', 230.03);
+INSERT INTO buglava.watch (id, is_digital, manufacturer, status, title, price) VALUES (11, true, 'Manufacturer100', 0, 'Watch10', 281.89);
